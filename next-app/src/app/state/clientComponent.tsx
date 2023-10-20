214de2bfcb767949d90df673733afdf312dc0060
AlexandruD L-Dell <AlexandruD@hotmail.com>
2023-10-20 22:10:20 +0300

    Added fetching data from redis with swr

--- next-app/src/app/state/clientComponent.tsx
+++ next-app/src/app/state/clientComponent.tsx
@@ -2,8 +2,17 @@
 import { GetDataWithSWR } from "@/lib/swr";
 
 const ClientComponent = () => {
-  const { data, error } = GetDataWithSWR(
+  interface RedisData {
+    id: string;
+    redisValue: string;
+  }
+
+  const { data: data, error: error } = GetDataWithSWR(
     "https://jsonplaceholder.typicode.com/todos/2"
+  );
+
+  const { data: dataApiRedis, error: errorApiRedis } = GetDataWithSWR(
+    "https://next-app-7fgzqv.bunnyenv.com/api/user?id=Testing%20ID"
   );
 
   console.log("swr_from_clientComponent.tsx", data, error);
@@ -12,7 +21,9 @@ const ClientComponent = () => {
     <div className="bg-stone-500 dark:bg-stone-700 p-10">
       <p>I am inside ClientComponent</p>
       <p>SWR value: {data ? data.title : "Zilch, nada, nothing"}</p>
-      <p>SWR Redis value: </p>
+      <p>
+        SWR Redis value: {dataApiRedis ? dataApiRedis.id : "Nothing fetched"}
+      </p>
     </div>
   );
 };
